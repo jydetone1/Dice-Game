7fe5b040d8c2d9ac6a8e4cda8431b0d7034b204b
jydetone1 <jydetone@gmail.com>
2025-05-01 10:59:47 +0300

    add text color

--- src/app/providers/MuiProvider.tsx
+++ src/app/providers/MuiProvider.tsx
@@ -4,11 +4,11 @@ import { ThemeProvider, CssBaseline } from '@mui/material';
 import { PropsWithChildren } from 'react';
 import { theme } from '../ui/theme';
 
-export function MuiProvider({ children }: PropsWithChildren) {
+export const MuiProvider = ({ children }: PropsWithChildren) => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
       {children}
     </ThemeProvider>
   );
-}
+};
